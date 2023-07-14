49cd84a09b45ecf547507bc6be0b850beda2ab0f
Bembit <bencemadar89@gmail.com>
2023-07-14 10:48:17 +0200

    added auth logic

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,19 +1,23 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 import { getStorage } from "firebase/storage";
+import { writable } from "svelte/store";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+// The apiKey in this configuration snippet just identifies your Firebase project on the Google servers.
+// It is not a security risk for someone to know it. 
 const firebaseConfig = {
-  apiKey: "AIzaSyAKld0rOuOoNC4XfmNynklNy9uZvcVw1fU",
-  authDomain: "sveltekit-app-fkit.firebaseapp.com",
-  projectId: "sveltekit-app-fkit",
-  storageBucket: "sveltekit-app-fkit.appspot.com",
-  messagingSenderId: "906165652545",
-  appId: "1:906165652545:web:31fb231de2e35f96668ac0"
+	apiKey: "AIzaSyAKld0rOuOoNC4XfmNynklNy9uZvcVw1fU",
+	authDomain: "sveltekit-app-fkit.firebaseapp.com",
+	projectId: "sveltekit-app-fkit",
+	storageBucket: "sveltekit-app-fkit.appspot.com",
+	messagingSenderId: "906165652545",
+	appId: "1:906165652545:web:31fb231de2e35f96668ac0"
 };
 
 // Initialize Firebase
@@ -21,3 +25,32 @@ export const app = initializeApp(firebaseConfig);
 export const db = getFirestore();
 export const auth = getAuth();
 export const storage = getStorage();
+
+// current firebase user store
+
+function userStore() {
+
+	let unsubscribe: () => void;
+
+	if(!auth || !globalThis.window) {
+		console.warn('Auth is not initialized or not in browser');
+		const { subscribe }	= writable<User | null>(null);
+		return {
+			subscribe,
+		}
+	}
+
+	const { subscribe } = writable(auth?.currentUser ?? null, (set) => {
+		unsubscribe = onAuthStateChanged(auth, (user) => {
+			set(user);
+		});
+
+		return () => unsubscribe();
+
+	});
+	return {
+		subscribe,
+	}
+}
+
+export const user = userStore();
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,5 +1,28 @@
 <script lang="ts">
 
+    import { auth } from "$lib/firebase";
+
+    import { user } from "$lib/firebase";
+
+    import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
+
+    async function signInWithGoogle() {
+        const provider = new GoogleAuthProvider();
+        // added custom param to always allow to select account
+        provider.setCustomParameters({ prompt: "select_account" });
+        const user = await signInWithPopup(auth, provider);
+        console.log(user);
+    }
+
 </script>
 
-<h2>Sign in with google</h2>
+<h2>Sign in</h2>
+
+{#if $user}
+    <h2 class="card-title">Welcome, {$user.displayName}</h2>
+    <button class="btn btn-danger" on:click={() => signOut(auth)}>Sign out</button>
+{:else}
+<!-- signinwithgoogle will remember the last google account and autosign in -->
+<!-- this should be ok, except if I delib logout, maybe to change users -->
+<button class="btn" on:click={signInWithGoogle}>Sign in with google</button>
+{/if}
