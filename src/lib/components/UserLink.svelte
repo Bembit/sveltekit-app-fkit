b20c5cd4f152526fe3ac0c62462f17334e17e1c3
Bembit <bencemadar89@gmail.com>
2023-07-16 14:08:28 +0200

    sort for links added

--- src/lib/components/UserLink.svelte
+++ src/lib/components/UserLink.svelte
@@ -6,6 +6,6 @@
 
 
 <a target="_blank" href={url} class="mb-2 stack w-full max-w-md text-center bg-base-300 flex justify-center items-center p-4 rounded-lg not-prose no-underline">
-  <img src={`/${icon}.png`} alt={icon} width="32" height="32" class="w-8" />
+  <!-- <img src={`/${icon}.png`} alt={icon} width="32" height="32" class="w-8" /> -->
   <span class="text-lg text-white font-bold">{title}</span>
 </a>
--- src/routes/[username]/edit/+page.svelte
+++ src/routes/[username]/edit/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     import { page } from "$app/stores";
     import AuthCheckMiddleware from "$lib/components/AuthCheckMiddleware.svelte";
+    import SortableList from "$lib/components/SortableList.svelte";
     import UserLink from "$lib/components/UserLink.svelte";
     import { db, userData, user } from "$lib/firebase";
     import {
@@ -36,6 +37,7 @@
     $: formIsValid = urlIsValid && titleIsValid;
 
   
+	// addlink form sometimes gets prepopulated with old data
     async function addLink(e: SubmitEvent) {
       const userRef = doc(db, "users", $user!.uid);
   
@@ -55,54 +57,63 @@
       showForm = false;
     }
 
-  
+	// create a confirmation component for deleting anything
+	// rn it's too easy to delete something by accident 
     async function deleteLink(item: any) {
-      const userRef = doc(db, "users", $user!.uid);
-      await updateDoc(userRef, {
-        links: arrayRemove(item),
-      });
+		const userRef = doc(db, "users", $user!.uid);
+		await updateDoc(userRef, {
+			links: arrayRemove(item),
+		});
     }
   
     function cancelLink() {
-      formData.set(formDefaults);
-      showForm = false;
+		formData.set(formDefaults);
+		showForm = false;
     }
-  
+
+	// Sortable List
+	function sortList(e: CustomEvent) {
+		const newList = e.detail;
+		const userRef = doc(db, "users", $user!.uid);
+		setDoc(userRef, { links: newList }, { merge: true });
+    }
+
+
 </script>
 
   <!-- <AuthCheckMiddleware> -->
 
-  <main class="max-w-xl mx-auto">
-    {#if $userData?.username == $page.params.username}
-      <h1 class="mx-2 text-2xl font-bold mt-8 mb-4 text-center">
-        Edit your Profile
-      </h1>
+	<main class="max-w-xl mx-auto">
+		{#if $userData?.username == $page.params.username}
+			<h1 class="mx-2 text-2xl font-bold mt-8 mb-4 text-center">
+				Edit your Profile
+			</h1>
 
       <!-- INSERT sortable list here -->
 
-      {#if showForm}
+	{#if showForm}
         <form
-          on:submit|preventDefault={addLink}
-          class="bg-base-200 p-6 w-full mx-auto rounded-xl"
-        >
-          <select
-            name="icon"
-            class="select select-sm"
-            bind:value={$formData.icon}
+			on:submit|preventDefault={addLink}
+			class="bg-base-200 p-6 w-full mx-auto rounded-xl"
+       	>
+			<select
+				name="icon"
+				class="select select-sm"
+				bind:value={$formData.icon}
           >
 
             {#each icons as icon}
               <option value={icon.toLowerCase()}>{icon}</option>
             {/each}
-          </select>
+          	</select>
 
-          <input
-            name="title"
-            type="text"
-            placeholder="Title"
-            class="input input-sm"
-            bind:value={$formData.title}
-          />
+			<input
+				name="title"
+				type="text"
+				placeholder="Title"
+				class="input input-sm"
+				bind:value={$formData.title}
+         	 />
 
           <input
             name="url"
@@ -138,18 +149,31 @@
 
         </form>
 
-      {:else}
-        <button
-          on:click={() => (showForm = true)}
-          class="btn btn-outline btn-info block mx-auto my-4"
-        >
-          Add a Link
-        </button>
+		{:else}
 
-        <a class="btn" href="/{$userData.username}">Back to profile</a>
+			<button
+				on:click={() => (showForm = true)}
+				class="btn btn-outline btn-info block mx-auto my-4"
+			>
+				Add a Link
+			</button>
 
-      {/if}
+			<a class="btn" href="/{$userData.username}">Back to profile</a>
+
+		{/if}
     {/if}
+
+	<SortableList list={$userData?.links} on:sort={sortList} let:item let:index>
+        <div class="group relative">
+         	 <UserLink {...item} />
+         	 <button
+				on:click={() => deleteLink(item)}
+				class="btn btn-xs btn-error invisible group-hover:visible transition-all absolute -right-6 bottom-10"
+           		>Delete</button
+          >
+        </div>
+	</SortableList>
+
   </main>
 
 <!-- </AuthCheckMiddleware> -->
